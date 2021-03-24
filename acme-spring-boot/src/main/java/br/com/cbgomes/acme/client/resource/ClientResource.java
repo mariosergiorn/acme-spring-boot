c1bec02feede74a6380b1ee15977159dc33789ab
Mario Sergio <49910760+mariosergiorn@users.noreply.github.com>
2021-03-24 19:04:36 -0300

    Add files via upload
    First commit

---
+++ acme-spring-boot/src/main/java/br/com/cbgomes/acme/client/resource/ClientResource.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package br.com.cbgomes.acme.client.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cbgomes.acme.client.domain.Client;
+import br.com.cbgomes.acme.client.repository.ClientRepository;
+import br.com.cbgomes.acme.service.exceptions.ClientNotFoundException;
+
+/**
+ * @author cbgomes
+ *
+ */
+@RestController
+@RequestMapping("/api/clients")
+public class ClientResource {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@DeleteMapping("/{id}")
+	public void deleteClient(@PathVariable("id") Long id) {
+		this.repository.deleteById(id);
+	}
+	
+	@PutMapping()
+	public void updateClient(@RequestBody Client client,
+		@RequestParam("id")Long id) throws Exception {
+		this.repository.save(this.repository.findById(id).orElseThrow(() -> new ClientNotFoundException(HttpStatus.BAD_REQUEST, "client not found")));
+	}
+	
+	@GetMapping("/id")
+	public Client getClientById(@PathVariable("id") Long id) {
+		return repository.findById(id).get();
+		
+	}
+	
+	@GetMapping
+	public List<Client> getAllClients(){
+		List<Client> clients = this.repository.findAll();
+		return clients;
+	}
+	
+	@PostMapping
+	public void createClient(@RequestBody Client client) {
+		this.repository.save(client);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ acme-spring-boot/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (1,'Carlos Barbosa','cgomes@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (2,'Mario Sergio','mario@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (3,'Paulo Sergio','paulo@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (4,'Lucas Pedra de Fogo','lucas@gmail.com', '83 9 9126-7778');
+INSERT INTO TB_CLIENTS (id,name,email,phone) VALUES (5,'Victor Assis','assis@gmail.com', '83 9 9126-7778');
